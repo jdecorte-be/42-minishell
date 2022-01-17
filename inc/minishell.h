7c181475ea2ce2343ec52ac570f37a12e8739c10
Lei jie Xu wu <lxu-wu@fu-r1-p5.s19.be>
2022-01-17 17:00:13 +0100

    17/01

--- inc/minishell.h
+++ inc/minishell.h
@@ -14,13 +14,13 @@
 # include <limits.h>
 # include <dirent.h>
 
-typedef struct s_direct
+typedef struct s_redirect
 {
 	t_list	*infile;
 	t_list	*outfile;
 	int		infd;
 	int		outfd;
-}	t_direct;
+}	t_redirect;
 
 typedef struct s_sep
 {
@@ -141,6 +141,6 @@ char	*ft_ecrase_q(char *word);
 
 int		ft_isprohibited(char *line);
 char	*ft_cutoff(char *str, size_t start, size_t len);
-void	ft_redirect(char *line);
+t_redirect	ft_redirect(char *line);
 
 #endif
--- src/utils/file_redirect.c
+++ src/utils/file_redirect.c
@@ -1,20 +1,35 @@
 #include "../../inc/minishell.h"
 
+void	nothing(void)
+{
+}
+
+
 char	*ft_chrredirect(char *line, char c)
 {
 	size_t	end;
+	size_t	start;
+	int		i;
 
+	i = 2;
 	end = 0;
-	if	(ft_strchr("<>", line[end]))
+	if (line[end] == '<')
 		end++;
-	while (ft_isspace(line[end]))
-		end++;
+	else
+		while (line[end] == '>' && i--)
+			end++;
+	if (ft_isspace(line[end]))
+		while (ft_isspace(line[end]))
+			end++;
+	else
+
+	start = end;
 	while (line[end] && !ft_isspace(line[end]))
 		end++;
-	return (ft_substr(line, 0, end));
+	return (ft_substr(line, 0, end));//remplacer le 0 par start; et end par end - start;
 }
 
-t_list	*ft_infile(char *line)
+t_list	*ft_file(char *line, char c)
 {
 	t_list	*lst;
 	size_t	i;
@@ -25,13 +40,16 @@ t_list	*ft_infile(char *line)
 	while (line[i])
 	{
 		// if (*line && *(line + 1) && ft_strncmp(*line, ">>", 1))
-		// if (*line && *(line + 1) && ft_strncmp(*line, "<<", 1))
-		if (line[i] && ft_strchr("<", line[i]))
+		// if (*line && *(line + 1) && ft_strncmp(line + i, "<<", 1))
+		// 	i++;
+		if (line[i] && line[i] == c && line[i + 1] && line[i + 1] != '<')
 		{
-			str = ft_chrredirect(line + i, '<');
+			str = ft_chrredirect(line + i, c);
 			ft_lstadd_back(&lst, ft_lstnew(str));
 			i += ft_strlen(str);
 		}
+		else if (line[i] && line[i + 1] && line[i] == '<' && line[i + 1] == '<')
+			i += 2;
 		else
 			i++;
 
@@ -39,12 +57,41 @@ t_list	*ft_infile(char *line)
 	return (lst);
 }
 
-void	ft_redirect(char *line)
+t_redirect	ft_init_redirect(void)
 {
-	t_direct	file;
+	t_redirect	file;
 
+	file.infile = 0;
+	file.outfile = 0;
+	file.infd = 0;
+	file.outfd = 0;
+	return (file);
+}
+
+t_redirect	ft_redirect(char *line)
+{
+	t_redirect	file;
+
+	file = ft_init_redirect();
 	if (!line)
-		return (0);
-	file.infile = ft_infile(line);
-	printf("%s|\n", (char *)file.infile->content);
+		return (file);
+	file.infile = ft_file(line, '<');
+	file.outfile = ft_file(line, '>');
+	while (file.infile)
+	{
+		printf("infile %s\n", file.infile->content);
+		// file.infd = open(file.infile->content, O_RDONLY);
+		// if (file.infd == -1)
+		// 	perror("open");
+		file.infile = ft_next(file.infile);
+	}
+	while (file.outfile)
+	{
+		printf("outfile %s\n", file.outfile->content);
+		// file.outfd = open(file.outfile->content, O_WRONLY | O_TRUNC | O_CREAT, 0644);
+		// if (file.outfd == -1)
+		// 	perror("open");
+		file.outfile = ft_next(file.outfile);
+	}
+	return (file);
 }
