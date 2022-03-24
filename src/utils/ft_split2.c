f29225efd91b35049ef4675ba3e6299e00e1e260
jdecorte42 <jdecorte42@Johns-MacBook-Air.local>
2022-03-24 17:12:18 +0100

    update

--- src/utils/ft_split2.c
+++ src/utils/ft_split2.c
@@ -86,7 +86,6 @@ char	**ft_split2(char *str, char *set)
 	if (!str && !*str)
 		return (0);
 	word = ft_count_words(str, set);
-	// printf("word = %zu\n", word);
 	tab = malloc(sizeof(char *) * (word + 1));
 	if (!tab)
 		ft_error(2);
