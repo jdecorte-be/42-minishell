8659491fba3308049d4137c5ce11fcd06b5fff1c
jdecorte42 <jdecorte42@Johns-MacBook-Air.local>
2022-03-24 17:48:00 +0100

    update

--- src/signal/signal.c
+++ src/signal/signal.c
@@ -1,15 +1,14 @@
 #include "../../inc/minishell.h"
 
-// void	newprompt(int sig)
-// {
-//     printf("\n");
-//     rl_on_new_line();
-//     rl_replace_line("", 0);
-//     rl_redisplay();
-// }
+void	newprompt(int sig)
+{
+    printf("\n");
+    rl_on_new_line();
+    rl_replace_line("", 0);
+    rl_redisplay();
 
-// void	sig_handler()
-// {
-//     signal(SIGINT, newprompt);
-// }
+void	sig_handler()
+{
+    signal(SIGINT, newprompt);
+}
 
